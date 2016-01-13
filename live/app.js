5d71daef31e5b54fd0c6773e2a77c0c356919dc7
Arya Solhi <aryasolhi@gmail.com>
2016-01-12 23:16:51 -0500

    Initial live site

---
+++ live/app.js
@@ -0,0 +1,8 @@
+(function(){
+    var live = angular.module('live', ['firebase']);
+    live.controller('AnnouncementController', ["$firebaseArray", function($firebaseArray){
+        var ref = new Firebase("https://deltahackslive.firebaseio.com/Announcements");
+        var a = this;
+        a.announcements = $firebaseArray(ref);
+    }]);
+})();
