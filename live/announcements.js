a1ba0180b962b502d95ae05979ad93538e84dd04
Arya Solhi <aryasolhi@gmail.com>
2016-01-15 19:14:52 -0500

    added form for adding announcements

---
+++ live/announcements.js
@@ -0,0 +1,32 @@
+(function(){
+    var live = angular.module('live', ['firebase']);
+    live.controller('AnnouncementController', ["$firebaseArray","$firebaseAuth","$firebaseObject", function($firebaseArray, $firebaseAuth,$firebaseObject){ 
+        var a = this;
+
+        var ref = new Firebase("https://deltahackslive.firebaseio.com");
+        var auth = $firebaseAuth(ref);
+        
+        a.login = function(){
+            auth.$authWithOAuthPopup("google").then(function(authData) {
+            }).catch(function(error) {
+                console.log("Authentication failed:", error);
+            });
+        }
+        a.submit = function(){
+            a.announcements.$add(a.text);
+            a.text = "";
+        }
+        auth.$onAuth(function(authData){
+            var obj = $firebaseObject(ref.child("users"))
+            obj.$loaded(function(){
+                obj[authData.uid] = 1;
+                obj.$save();
+            })
+        });
+        
+        a.loggedin = function(){
+            return auth.$getAuth();
+        }
+        a.announcements = $firebaseArray(ref.child("Announcements"));
+    }]);
+})();
