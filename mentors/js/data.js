5716d7d058935bc13adbcd74eb7c14766138d9b8
alicj <zichen.jiang@yahoo.com>
2018-01-22 14:06:44 -0500

    add mentors page

---
+++ mentors/js/data.js
@@ -0,0 +1,35 @@
+// If more examples/projects are to be added, 
+// please add them at the END of exampleData
+// If a new year's projects are added,
+// add the new year to the TOP of yearData
+
+var yearData =[
+	2018
+]
+
+var exampleData = [
+	{ 
+		name: "Alic Jiang", 
+		year: 2018, 
+		imgPath: "images/2018/mentor1.png", 
+		desc: `A cool mentor`,
+		skills: ["javascript", "html"],
+		slack: "D61CLF5TP"
+	},
+	{ 
+		name: "Natalie Chin", 
+		year: 2018, 
+		imgPath: "images/2018/mentor2.png", 
+		desc: `A cool mentor`,
+		skills: ["javascript", "html"],
+		slack: "slack id"
+	},
+	{ 
+		name: "Abhay", 
+		year: 2018, 
+		imgPath: "images/2018/mentor3.png", 
+		desc: `A cool mentor`,
+		skills: ["javascript", "html"],
+		slack: "slack id"
+	},
+];
