c191a70917eb983a27bb529eb69dadbff85b1f71
alicj <zichen.jiang@yahoo.com>
2018-01-26 11:48:06 -0500

    add workshop info

--- workshops/js/data.js
+++ workshops/js/data.js
@@ -5,29 +5,28 @@
 
 var exampleData = [
 	{ 
-		name: "Digital Conversations– AI and Voice First Experiences", 
-		author: "TD",
-		time: "TBD",
-		location: "ETB 228",
+		name: "Build your own AI assistant using Dialogflow", 
+		author: "RBC",
+		time: "3:15PM",
+		location: "ETB 230",
 		desc: `
-A host of technologies are emerging that broadly fall under the umbrella of artificial intelligence (AI) and are revolutionizing how we design and deliver digital experiences. In this talk, we'll discuss how as AI evolves the conversations we have will give us better opportunities to meet customer needs directly through new connected products that go well beyond smartphones and the AI frameworks currently available within this space.
-<br/> <br/>
-The second part of the discussion will focus on the traction voice platforms are getting. When we use Voice Interfaces, the interaction is primarily carried out by just speaking and listening. This does not mean that we have to disregard everything we have successfully applied to previous interfaces; we need to adapt our process for the nuances of voice-driven interfaces, including conversational interactions, the lack of a screen and the help we can get from the use of some AI capabilities.  The talk will cover the current ecosystem of voice interfaces and also the trends in each of these platforms, including Amazon Alexa, Google Home and Microsoft Cortana. We will review some of the self-service APIs and tools that can help us to create new voice-driven capabilities for each platform. And we will use Alexa as main device for live demos and live coding.
-		`,
-		slack: "U8VB173M4"
+The world of virtual assistants powered by AI is growing — Siri, Cortana, Alexa, Google Assistant to name just a few.  However, did you know it’s quite easy to build your own AI assistant too?  In this workshop, you will learn how to create your own chatbot using Dialogflow (natural language processing engine) to connect to users with Google Assistant via Google Home.
+<br/>
+Speaker: Pratik Mehta, Senior Technical Systems Analyst		`,
+		slack: ""
 	},
 	{ 
-		name: "ArcelorMittal Dofasco", 
-		author: "ArcelorMittal Dofasco",
-		time: "TBD",
-		location: "ETB 230",
-		desc: `This session will focus on the new technologies, modernization, and continuous improvement activities taking place at ArcelorMittal Dofasco.`,
+		name: "Bridging the Gap – The Journey from Academia to the Professional World - Exploring Object Oriented Development with Unity Game Engine", 
+		author: "FDM Group",
+		time: "3:15PM",
+		location: "ETB 228",
+		desc: `The workshop is aiming to create a 3D ‘Blind Bridge Building’ Game (3rd Person Perspective) with Unity Game Engine. We are going to use Playmaker Finite-State-Machine and JavaScript or C# code to develop and deploy the game. We will step through certain parts of the game to demonstrate core OOD principles and how the Algorithms work, in real-time, through the Playmaker FSM. In addition, we will interact with the Algorithm, game-objects, and environment and see the effects in real-time.`,
 		slack: ""
 	},
 	{ 
 		name: "Legacy Technologies in the Modern World", 
 		author: "Rogers",
-		time: "TBD",
+		time: "3:35PM",
 		location: "ETB 230",
 		desc: `Legacy Technologies you weren't expecting in 2018 and how to mix the old with the new technologies`,
 		slack: ""
@@ -35,7 +34,7 @@ The second part of the discussion will focus on the traction voice platforms are
 	{ 
 		name: "Prioritizing Machine Learning", 
 		author: "Indellient",
-		time: "TBD",
+		time: "4:00PM",
 		location: "ETB 228",
 		desc: `A deep dive into how students can mesh tech with business and what skills they need to build a marketable product.`,
 		slack: ""
@@ -43,15 +42,23 @@ The second part of the discussion will focus on the traction voice platforms are
 	{ 
 		name: "Personalized Development At Scale (with Books and Robots!)", 
 		author: "Kobo",
-		time: "TBD",
+		time: "4:00PM",
 		location: "ETB 230",
 		desc: `Our tech stack is as diverse and experimental as our digital catalog of books. Our R&D folks are a special group - In the morning, we take a splash of RUM with our customer feedback to kick off the next day of continuous improvement. We make our robots come alive to double check the software we write and the hardware we design, and we show our work to millions of people each day. Join us in our workshop where we’ll talk about the fascinating revolution going on in the book industry, our experimental api’s, developer motivation, robot motivation, and how we intertwine personalization at a global scale.`,
 		slack: ""
 	},
 	{ 
+		name: "StdLib", 
+		author: "StdLib",
+		time: "4:45PM",
+		location: "ETB 126",
+		desc: `TBA`,
+		slack: ""
+	},
+	{ 
 		name: "Leveraging AR glasses/phone/tablet to identify process equipment and to display general information for a specific piece of equipment", 
 		author: "Hatch",
-		time: "TBD",
+		time: "4:45PM",
 		location: "ETB 228",
 		desc: `Vision: <br/>
 A plant maintenance worker is walking through an operating industrial facility. As they move their AR glasses/phone/tablet is continuously identifying the process equipment around them and displaying a general information for that specific equipment such as name and alarm conditions or required maintenance. The worker notices one piece of equipment (i.e. pump) indicating a problem and requests more detailed information such as time to next maintenance, data from IoT sensors (i.e. vibration, temperature), operating instructions, flow curves, etc). The worker reviews the data and decides to submit a maintenance request to resolve the issue.
@@ -64,44 +71,37 @@ UI for presenting information in an easy to understand format that does not inte
 		slack: ""
 	},
 	{ 
-		name: "StdLib", 
-		author: "StdLib",
-		time: "TBD",
-		location: "ETB 126",
-		desc: ``,
-		slack: ""
+		name: "Digital Conversations– AI and Voice First Experiences", 
+		author: "TD",
+		time: "5:45PM",
+		location: "ETB 228",
+		desc: `
+A host of technologies are emerging that broadly fall under the umbrella of artificial intelligence (AI) and are revolutionizing how we design and deliver digital experiences. In this talk, we'll discuss how as AI evolves the conversations we have will give us better opportunities to meet customer needs directly through new connected products that go well beyond smartphones and the AI frameworks currently available within this space.
+<br/> <br/>
+The second part of the discussion will focus on the traction voice platforms are getting. When we use Voice Interfaces, the interaction is primarily carried out by just speaking and listening. This does not mean that we have to disregard everything we have successfully applied to previous interfaces; we need to adapt our process for the nuances of voice-driven interfaces, including conversational interactions, the lack of a screen and the help we can get from the use of some AI capabilities.  The talk will cover the current ecosystem of voice interfaces and also the trends in each of these platforms, including Amazon Alexa, Google Home and Microsoft Cortana. We will review some of the self-service APIs and tools that can help us to create new voice-driven capabilities for each platform. And we will use Alexa as main device for live demos and live coding.
+		`,
+		slack: "U8VB173M4"
 	},
 	{ 
-		name: "Build your own AI assistant using Dialogflow", 
-		author: "RBC",
-		time: "TBD",
+		name: "Your Role in the Digital Transformation", 
+		author: "ArcelorMittal Dofasco",
+		time: "5:45PM",
 		location: "ETB 230",
-		desc: `
-The world of virtual assistants powered by AI is growing — Siri, Cortana, Alexa, Google Assistant to name just a few.  However, did you know it’s quite easy to build your own AI assistant too?  In this workshop, you will learn how to create your own chatbot using Dialogflow (natural language processing engine) to connect to users with Google Assistant via Google Home.
-<br/>
-Speaker: Pratik Mehta, Senior Technical Systems Analyst		`,
+		desc: `This session will focus on the new technologies, modernization, and continuous improvement activities taking place at ArcelorMittal Dofasco. `,
 		slack: ""
 	},
 	{ 
 		name: "Working with gestures", 
 		author: "Xesto",
-		time: "TBD",
+		time: "6:05PM",
 		location: "ETB 230",
 		desc: `We'll get you set up to make really cool interactive, touchless applications!`,
 		slack: ""
 	},
 	{ 
-		name: "Bridging the Gap – The Journey from Academia to the Professional World - Exploring Object Oriented Development with Unity Game Engine", 
-		author: "FDM Group",
-		time: "TBD",
-		location: "ETB 228",
-		desc: `The workshop is aiming to create a 3D ‘Blind Bridge Building’ Game (3rd Person Perspective) with Unity Game Engine. We are going to use Playmaker Finite-State-Machine and JavaScript or C# code to develop and deploy the game. We will step through certain parts of the game to demonstrate core OOD principles and how the Algorithms work, in real-time, through the Playmaker FSM. In addition, we will interact with the Algorithm, game-objects, and environment and see the effects in real-time.`,
-		slack: ""
-	},
-	{ 
 		name: "Jump start your hackathon project (with NodeJS, APIs, and git)", 
 		author: "Jimmy Wang",
-		time: "TBD",
+		time: "6:25PM",
 		location: "ETB 228",
 		desc: `Learn express, node, integrating APIs and web technologies to help get your hack started. Beginner workshop. Targeted towards first time hackers.`,
 		slack: ""
@@ -109,7 +109,7 @@ Speaker: Pratik Mehta, Senior Technical Systems Analyst		`,
 	{ 
 		name: "Intro to Game Dev", 
 		author: "Justin Licari",
-		time: "TBD",
+		time: "6:25PM",
 		location: "ETB 230",
 		desc: `The workshop will go over Unity basics to give people an idea on how to start making a 3D game. It'll be an interactive coding/design tutorial, with concepts discussed being physics, controllers, gameObjects, Unity editor, C# scripting in Monodevelop, and basic AI in games
 		<br/>
